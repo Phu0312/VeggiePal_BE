8c9cfa2c05e0cdf4dffa21d03a5d3fcd7b572b2d
Trương Hoàng Phú <hoangphulk2005@gmail.com>
2026-09-09 20:36:05 +0700

    initial project

---
+++ src/main/java/com/veggiepal/VeggiepalApplication.java
@@ -0,0 +1,13 @@
+package com.veggiepal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeggiepalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeggiepalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veggiepal/VeggiepalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veggiepal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeggiepalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
