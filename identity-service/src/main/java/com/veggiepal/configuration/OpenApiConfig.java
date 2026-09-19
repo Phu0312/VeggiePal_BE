23ea426706715a6f1a61acf98b798e647bf154b7
Log <nguyennamlong0411@gmail.com>
2026-09-19 22:33:38 +0700

    chore(identity): add bearer validation in swagger

--- identity-service/src/main/java/com/veggiepal/configuration/OpenApiConfig.java
+++ identity-service/src/main/java/com/veggiepal/configuration/OpenApiConfig.java
@@ -5,12 +5,17 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class OpenApiConfig {
+
+    private static final String BEARER_AUTH = "bearerAuth";
 
     @Bean
     public OpenAPI veggiePalOpenAPI() {
@@ -28,6 +33,17 @@ public class OpenApiConfig {
                                         .url("/api")
                                         .description("API Gateway")
                         )
-                );
+                )
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        BEARER_AUTH,
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH));
     }
 }
